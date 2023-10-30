fadc505f36be81c0ee41f1990a0faf4a4f5a18ea
Hardiv Harshakumar <hardiv@Hardiv-Laptop>
2023-10-30 20:45:11 +0000

    [ss3722][hh1222][sk4122][sk2922] Update syscall_handler in syscall.c (made function setup_args)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,8 +12,23 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+/* Sets up Arguments */
+static void setup_args(void *esp) {
+	int syscall_no = *(int *)esp;
+	int argc = *((int *)esp + sizeof(int));   // 4 byte intervals
+
+	/* Need to check syntax for this*/
+	char **argv = *(char **)(&argc + sizeof(char**));
+	for (int i = 0; i < argc; i++)
+	{
+		/* need to increment esp here and get arguments */
+
+	}
+}
+
+
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f)
 {
 	/* Verification of User Provided Pointers, and Dereferences */
 	/* TODO: Change to Method 2 */
@@ -21,7 +36,11 @@ syscall_handler (struct intr_frame *f UNUSED)
 	{
 		if (pagedir_get_page(thread_current ()->pagedir, f->frame_pointer) != NULL)
 		{
-			f->frame_pointer = * (void *)f->frame_pointer;
+			/* De-Referencing Pointer */
+			f->frame_pointer = (void *) (*(uint32_t *)f->frame_pointer);
+
+			setup_args(f->esp);
+
 		}
 	}
 
