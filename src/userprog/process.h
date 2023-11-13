d8a0cca820cb69c2268348d58360aeda9d8c52e7
Simran Kaware <sk4122@ic.ac.uk>
2023-11-13 21:04:47 +0000

    [sk4122] Argument Passing is correct

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -18,27 +18,11 @@ tid_t process_execute (const char *);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-void rs_manager_init (struct rs_manager *, struct thread *);
-struct rs_manager* get_child (struct thread *parent, tid_t tid);
 
 /* A relationship manager for user processes. 
 
    Each user process has an rs_manager, used to store its children
    and its exit status, and also some synchronization primitives. */
-struct rs_manager 
-{
-    struct rs_manager *parent_rs_manager;   /* Pointer to parent rs_manager. */
-    struct list children;                   /* List of all child rs_manager. */
-    struct list_elem child_elem;            /* List elem for children list.  */ 
 
-    struct thread *thread;                  /* Pointer to actual process. */
-    
-    struct semaphore child_exit_sema;       /* Semaphore for process exit. */
-    struct semaphore child_load_sema;       /* Semaphore for process load. */
-
-    bool load_success;                      /* Boolean for load status. */
-    bool error;                             /* Boolean for exit error status. */
-    int exit_status;                        /* Exit status of process. */
-};
 
 #endif /* userprog/process.h */
